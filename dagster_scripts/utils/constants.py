9cd0425f189eb20de5d7e86e4ba883ae8c5e003d
Adam Samuel <adamsamuel18@gmail.com>
2023-05-04 10:57:30 +0100

    added profiles.yml secrets

--- dagster_scripts/utils/constants.py
+++ dagster_scripts/utils/constants.py
@@ -1,5 +1,5 @@
 from dagster._utils import file_relative_path
 
 DBT_PROJECT_DIR = file_relative_path(__file__, "../../dbt")
-DBT_PROFILES_DIR = file_relative_path(__file__, "../../../../.dbt")
+DBT_PROFILES_DIR = file_relative_path(__file__, "../../dbt")
 DBT_CONFIG = {"project_dir": DBT_PROJECT_DIR, "profiles_dir": DBT_PROFILES_DIR}
